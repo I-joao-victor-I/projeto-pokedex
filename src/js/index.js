3d391234c2dcbf7aae8b4affbbc5cb7ea742ae24
João Victor <127161710+I-joao-victor-I@users.noreply.github.com>
2023-09-16 19:00:55 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,16 @@
+
+const botaoAlterarTema = document.getElementById("botao-alterar-tema");
+const body = document.querySelector("body");
+const imagemBotaoTrocaDeTema = document.querySelector(".imagem-botao");
+
+botaoAlterarTema.addEventListener("click", () => {
+    const modoEscuroEstaAtivo = body.classList.contains("modo-escuro");
+
+    modoEscuroEstaAtivo.classList.toggle("modo-escuro");
+
+    if (modoEscuroEstaAtivo) {
+        imagemBotaoTrocaDeTema.setAttribute("src", "./src/imagens/sun.png");
+    } else {
+        imagemBotaoTrocaDeTema.setAttribute("src", "./src/imagens/moon.png");
+    }
+});
